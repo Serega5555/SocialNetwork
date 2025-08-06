367d402cfea39d5be82ee584265fbbcd473df70c
Serega5555 <lazarpalace@gmail.com>
2025-08-06 13:46:41 +0300

    Create UserDataUpdateView.cs

---
+++ SocialNetwork/PLL/Views/UserDataUpdateView.cs
@@ -0,0 +1,40 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserDataUpdateView
+    {
+        UserService userService;
+        public UserDataUpdateView(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Show(User user)
+        {
+            Console.Write("Имя:");
+            user.FirstName = Console.ReadLine();
+
+            Console.Write("Фамилия:");
+            user.LastName = Console.ReadLine();
+
+            Console.Write("Фото:");
+            user.Photo = Console.ReadLine();
+
+            Console.Write("Любимый фильм:");
+            user.FavoriteMovie = Console.ReadLine();
+
+            Console.Write("Любимая книга:");
+            user.FavoriteBook = Console.ReadLine();
+
+            this.userService.Update(user);
+
+            SuccessMessage.Show("Профиль обновлён!");
+        }
+    }
+}
